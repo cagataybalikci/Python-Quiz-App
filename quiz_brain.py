ed18cdc75cbe52a2db5551c362f8594e00f5520b
Çağatay Balıkçı <30704478+cagataybalikci@users.noreply.github.com>
2021-04-19 22:19:45 +0300

    Finished App.

--- quiz_brain.py
+++ quiz_brain.py
@@ -11,18 +11,17 @@ class QuizBrain:
         return self.question_number < len(self.question_list)
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        q_text = html.unescape(current_question.text)
-        user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
-        self.chech_answer(user_answer, current_question.answer)
+        q_text = html.unescape(self.current_question.text)
+        return f"Q.{self.question_number}: {q_text}"
+        # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
+        # self.chech_answer(user_answer, current_question.answer)
 
-    def chech_answer(self, user_answer, correct_anwer):
-        if user_answer.lower() == correct_anwer.lower():
-            print("You got it right!")
+    def chech_answer(self, user_answer):
+        correct_answer = self.current_question.answer
+        if user_answer.lower() == correct_answer.lower():
             self.score += 1
+            return True
         else:
-            print("That's wrong.")
-        print(f"The correct answer was: {correct_anwer}")
-        print(f"Your current score  is: {self.score}/{self.question_number}")
-        print("\n")
+            return False
