d2d5040ed77e4101c81f00022cedf78593d577b9
Çağatay Balıkçı <30704478+cagataybalikci@users.noreply.github.com>
2021-04-19 21:14:47 +0300

    Api added.

---
+++ quiz_brain.py
@@ -0,0 +1,28 @@
+import html
+
+
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_list = q_list
+        self.question_number = 0
+        self.score = 0
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        q_text = html.unescape(current_question.text)
+        user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
+        self.chech_answer(user_answer, current_question.answer)
+
+    def chech_answer(self, user_answer, correct_anwer):
+        if user_answer.lower() == correct_anwer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_anwer}")
+        print(f"Your current score  is: {self.score}/{self.question_number}")
+        print("\n")
